03852d67b4a86c5416c0b9d9c0886eb4544c9613
Brayan González <brayan.a.gonzalez@gmail.com>
2018-09-11 11:52:25 -0300

    Added zshrc symlink

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ ln -sf $HOME/.dotfiles/vimrc $HOME/.vimrc
 
 # Create a symlink to i3/config
 ln -sf $HOME/.dotfiles/i3config $HOME/.i3/config
+
+# Create a symlink to zshrc
+ln -sf $HOME/.dotfiles/zshrc $HOME/.zshrc

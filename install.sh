44ea476793b8f8536e60ef3e50aef5d0ddfec248
Brayan Gonzalez <bgonzalez@acid.cl>
2018-09-13 09:33:05 -0300

    Added vscode settings

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Create a symlink to vimrc
-ln -sf $HOME/.dotfiles/vimrc $HOME/.vimrc
-
 # Create a symlink to i3/config
 ln -sf $HOME/.dotfiles/i3config $HOME/.i3/config
+
+# Create a symlink ti vscode settings
+ln -sf $HOME/.dotfiles/vscode_settings.json $HOME/.config/Code/User/settings.json
